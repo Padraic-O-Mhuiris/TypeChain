df5bcbade7ac3353434158559b48b9958a1c6c5a
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-04-28 17:17:12 +0100

    remove comments

--- packages/hardhat/src/config.ts
+++ packages/hardhat/src/config.ts
@@ -3,39 +3,12 @@ import { HardhatConfig } from 'hardhat/types'
 import { HardhatTypechainConfig } from './types'
 
 export function getDefaultTypechainConfig(config: HardhatConfig): HardhatTypechainConfig {
-  // const model = {
-  //   principalToken: { contract: 'Tranche.sol', schema: z.object({ xxx: z.literal('2') }) },
-  // } as const
-
-  // const generate: GenerateStatics<typeof model> = async () => ({
-  //   categories: ['principalToken'],
-  //   addresses: {
-  //     principalToken: [randomTagAddress('principalToken')],
-  //   },
-  //   contracts: {
-  //     principalToken: 'Tranche.sol',
-  //   },
-  //   metadata: {
-  //     principalToken: {
-  //       [randomTagAddress('principalToken')]: {
-  //         xxx: '2',
-  //       },
-  //     },
-  //   },
-  // })
-
-  // const statics: StaticsConfig = {
-  //   model,
-  //   generate,
-  // }
-
   const defaultConfig: HardhatTypechainConfig = {
     outDir: 'typechain-types',
     target: 'ethers-v5',
     alwaysGenerateOverloads: false,
     discriminateTypes: false,
     tsNocheck: false,
-    //statics,
   }
 
   return {
